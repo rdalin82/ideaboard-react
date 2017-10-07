2ce177ff603866ee48156763228bfd7f26e591d1
rdalin82 <rdalin@benchprep.com>
2017-10-07 09:54:37 -0500

    idea form

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,32 +1,64 @@
 import React, { Component } from 'react'
-import axios from 'axios' 
+import axios from 'axios'
+import update from 'immutability-helper'
 import Idea from './Idea'
+import IdeaForm from './IdeaForm'
 
-class IdeasContainer extends Component { 
+class IdeasContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      ideas: [] 
+      ideas: []
     }
   }
   render() {
-		return (
-			<div>
-        { this.state.ideas.map((idea) => {
-          return( <Idea idea={idea} key={idea.id} /> )
-        })}
-			</div>
+    return (
+      <div>
+        <button className="newIdeaButton" onClick={this.addNewIdea} >
+          New Idea
+        </button>
+        <div>
+	        { this.state.ideas.map((idea) => {
+            if(this.state.editingIdeaId === idea.id) {
+              return(<IdeaForm idea={idea} key={idea.id} />)
+            } else {
+              return(<Idea idea={idea} key={idea.id} /> )
+            }
+         })}
+		  	</div>
+      </div>
 		);
   }
-  componentDidMount() { 
+  componentDidMount() {
     axios.get('http://localhost:3001/api/v1/ideas.json')
-      .then(response=> { 
+      .then(response=> {
         console.log(response)
-        this.setState({ideas: response.data}) 
+        this.setState({ideas: response.data})
       })
       .catch(error => console.log(error))
-  } 
+  }
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/ideas',
+      { idea:
+        {
+          title: '',
+          body: '',
+        }
+      }
+    ).then(response => {
+        console.log(response)
+        const ideas = update(this.state.ideas, {
+          $splice: [[0, 0, response.data]]
+        })
+        this.setState({
+          ideas: ideas,
+          editingIdeaId: response.data.id
+        })
+    })
+      .catch(error=> console.log(error))
+  }
 }
 
 
-export default IdeasContainer 
+export default IdeasContainer
